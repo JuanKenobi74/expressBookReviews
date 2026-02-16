28c388fc1b9300cf470a41a72105138d1e993853
JuanKenobi74 <cadillacblack74@gmail.com>
2026-02-15 21:18:39 -0600

    added code to enable asychronous request/response

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,6 @@
 // router/general.js
 const express = require('express');
+const axios = require('axios').default;
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -26,6 +27,49 @@ public_users.post("/register", (req,res) => {
   return res.status(400).json({message: "Unable to register user. Please provide username and password"});
 });
 
+// async/await with Axios methods
+public_users.get("/async/books", async (req, res) => {
+  try {
+    const response = await axios.get("http://localhost:5000");
+    return res.send(JSON.stringify(response.data, null, 4));
+  } catch (error) {
+    console.error("Error fetching books:", error.message);
+    return res.status(500).json({ message: "Error retrieving book list" });
+  }
+});
+
+public_users.get("/async/isbn/:isbn", async (req, res) => {
+  const isbn = req.params.isbn;
+  try {
+    const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+    return res.send(JSON.stringify(response.data, null, 4));
+  } catch (error) {
+    console.error("Error fetching book details:", error.message);
+    return res.status(500).json({ message: "Error retrieving book details" });
+  }
+});
+
+public_users.get("/async/author/:author", async (req, res) => {
+  const author = req.params.author;
+  try {
+    const response = await axios.get(`http://localhost:5000/author/${author}`);
+    return res.status(200).send(JSON.stringify(response.data, null, 4));
+  } catch (error) {
+    console.error("Error fetching book details:", error.message);
+    return res.status(500).json({ message: "Error retrieving book details" });
+  }
+});
+
+public_users.get("/async/title/:title", async (req, res) => {
+  const title = req.params.title;
+  try {
+    const response = await axios.get(`http://localhost:5000/title/${title}`);
+    return res.status(200).send(JSON.stringify(response.data, null, 4));
+  } catch (error) {
+    console.error("Error fetching book details:", error.message);
+    return res.status(500).json({ message: "Error retrieving book details" });
+  }
+})
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   // return all books in books object
@@ -36,8 +80,10 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   // extract ISBN from request params
   const isbn = req.params.isbn;
-  // return book details for the given ISBN in JSON format
-  return res.send(JSON.stringify(books[isbn], null, 4));
+  // if books[isbn] return book details for the given ISBN in JSON format
+  if (books[isbn]) {
+    return res.send(JSON.stringify(books[isbn], null, 4));
+  }
  });
   
 // Get book details based on author
@@ -46,7 +92,7 @@ public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
   let booksKeys = Object.values(books);
   let filteredBooks = booksKeys.filter(book => book.author === author);
-  return res.send(JSON.stringify(filteredBooks, null, 4));
+  return res.status(200).send(JSON.stringify(filteredBooks, null, 4));
 });
 
 // Get all books based on title
